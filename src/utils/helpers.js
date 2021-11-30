97b230cc85c940fc1735bd6e8ca6c1468e5b4c55
Sebastian C <sebastian9111@gmail.com>
2021-11-30 23:05:13 +0200

    mend

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,10 @@
 
 const csvToArray = (text = '', delim = ',') => {
+  text = text.replace('\r', '')
   const headers = text.slice(0, text.indexOf('\n')).split(delim)
-  const rows = text.slice(text.indexOf('\n') + 1).split('\n')
+  const rows = text.slice(text.indexOf('\n') + 1).split('\r\n')
+
+  console.log(headers)
 
   const newArray = rows.map((row) => {
     const values = row.split(delim)
