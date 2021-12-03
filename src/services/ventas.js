3e874b8672663cdb1cb9ccb544e793f5b43f3536
Yamidy <dayyelaro@unal.edu.co>
2021-12-02 21:24:59 -0500

    ./services/ventas.js con las funciones para llamar a la api de ventas

---
+++ src/services/ventas.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+const registrarDetalleVentas = async (data) => {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  try {
+    await axios.post(
+      'http://localhost:8087/api/v1/detalleVentas/',
+      JSON.stringify(data),
+      { headers }
+    )
+  } catch {
+    alert('no se pudo ingresar los detalles de la venta')
+  }
+}
+
+const registrarVenta = async (data) => {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  try {
+    await axios.post(
+      'http://localhost:8087/api/v1/ventas/',
+      JSON.stringify(data),
+      { headers }
+    )
+    alert('venta registrada con éxito')
+  } catch {
+    alert('no se pudo ingresar la venta')
+  }
+}
+
+const buscarProducto = async (codigoProducto) => {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  const { data } = await axios.get(
+    'http://localhost:8085/api/v1/productos/' + codigoProducto,
+    { headers }
+  )
+  return data
+}
+
+export default { registrarDetalleVentas, registrarVenta, buscarProducto }
